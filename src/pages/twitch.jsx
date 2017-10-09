04c9862c17362fd79b89b6f52dc912947b16cf26
Luke Burgess-Yeo <luke.burgess94@gmail.com>
2017-10-09 12:08:56 +0100

    Displaying results from Twitch API

--- src/pages/twitch.jsx
+++ src/pages/twitch.jsx
@@ -8,28 +8,29 @@ class Broadcasters extends React.Component {
     this.state = {
       streams: ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"],
       loading: true,
-      result: null
+      result: []
     }
   }
 
   componentDidMount = () => {
-    axios
-      .get('https://cors-anywhere.herokuapp.com/https://wind-bow.gomix.me/twitch-api/ESL_SC2')
-      .then(result => {
-        console.log(result);
-        this.setState({
-          result: result,
-          loading: false
+    this.state.streams.forEach(stream => {
+      axios
+        .get('https://api.twitch.tv/kraken/streams/' + stream + '?client_id=ht7j8oqlzyu29r9mpn3tqlp4xii7zl')
+        .then(res => {
+          console.log(res);
+          this.setState(prev => ({
+            result: [...prev.result, { title: stream, data: res.data }]
+          }));
+        })
+        .catch(err => {
+          this.setState({
+            loading: false,
+            result: "An error occoured getting Twitch streams."
+          });
+          console.log(err);
         });
-      })
-      .catch(err => {
-        this.setState({
-          loading: false,
-          result: "An error occoured getting Twitch streams."
-        });
-        console.log(err);
-      });
-
+    })
+    this.setState({ loading: false });
   }
 
   render() {
@@ -37,7 +38,14 @@ class Broadcasters extends React.Component {
       <div>
         <h3>Broadcasters</h3>
         {(this.state.loading) && <div style={{ "textAlign": "center", "fontSize": 56 + "px" }}><FontAwesome name='refresh' spin={true} /></div>}
-        {(this.state.result !== null) && JSON.stringify(this.state.result)}
+        {(this.state.result.length === this.state.streams.length) && this.state.result.map((value, index) => (
+          <div className="wikiWrap" key={index}>
+            <a className="wikiCard" href={value.data._links.self} target="_blank">
+              <h3>{value.title}</h3>
+              <p>{(value.data.stream) ? value.data.stream.channel.status : "Offline"}</p>
+            </a>
+          </div>
+        ))}
       </div>
     )
   }
