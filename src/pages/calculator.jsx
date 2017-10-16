5fceccce7de5d0a3ac90174ca7f817cc0bf0eef1
Luke Burgess <luke.burgess94@gmail.com>
2017-10-16 23:42:12 +0100

    Refactored and finished calculator

--- src/pages/calculator.jsx
+++ src/pages/calculator.jsx
@@ -6,48 +6,47 @@ class Calc extends React.Component {
     super(props);
     this.state = {
       opp: '',
-      value: '0',
-      result: '0'
+      value: '',
+      result: null
     }
   }
 
   Clear = () => {
     this.setState({
       opp: '',
-      value: '0',
-      result: '0'
+      value: '',
+      result: null
     });
   }
 
-  Evaluate = () => {
-    if (this.state.value === '-') return;
-    switch (this.state.result[0]) {
+  updateResult = () => {
+    if (this.state.value === '-') {
+      return;
+    }
+    if (this.state.result === null) {
+      this.setState(p => ({
+        result: +p.value
+      }));
+    }
+    switch (this.state.opp) {
       case '*':
         this.setState(p => ({
-          opp: '=',
-          value: +p.result.substring(1) * +p.value,
-          result: '0'
+          result: p.result * +p.value
         }));
         break;
       case '/':
         this.setState(p => ({
-          opp: '=',
-          value: +p.result.substring(1) / +p.value,
-          result: '0'
+          result: p.result / +p.value
         }));
         break;
       case '+':
         this.setState(p => ({
-          opp: '=',
-          value: +p.result.substring(1) + +p.value,
-          result: '0'
+          result: p.result + +p.value
         }));
         break;
       case '-':
         this.setState(p => ({
-          opp: '=',
-          value: +p.result.substring(1) - +p.value,
-          result: '0'
+          result: p.result - +p.value
         }));
         break;
       default:
@@ -56,51 +55,44 @@ class Calc extends React.Component {
   }
 
   updateValue = (val) => {
-    if (this.state.value === '0' || this.state.opp === '=') {
-      val = ((val === '.') ? '0' : '') + val;
-      this.setState({
-        value: val,
-        opp: ''
-      });
-    } else if (this.state.opp !== '') {
-      this.setState(p => ({
-        opp: '',
-        value: val,
-        result: p.opp + p.value
-      }));
-    } else {
-      this.setState(p => ({
-        value: p.value + val
-      }));
+    if (+this.state.value === this.state.result) {
+      this.setState({ value: '' });
     }
+    this.setState(p => ({
+      value: p.value + val
+    }));
   }
 
   updateOpp = (op) => {
-    if (this.state.value === '0' && op === '-') {
+    if ((this.state.value === '' || this.state.opp !== '') && op === '-') {
       this.setState({ value: '-' });
-    } else if (this.state.opp !== '' && op === '-') {
-      this.setState(p => ({
-        opp: '',
-        value: '-',
-        result: p.opp + p.value
-      }));
-    } else if (this.state.value === '0' || (this.state.value === '0' && isNaN(this.state.result[0]))) {
       return;
-    } else if (this.state.opp === '') {
-      this.setState({ opp: op });
     }
-    if (this.state.opp === '=' || this.state.value !== '0') {
-      this.Evaluate();
-      this.setState({ opp: op });
+    if (this.state.value === '' && this.state.result === '0') {
+      return;
     }
+    this.updateResult();
+    this.setState({ opp: op });
+  }
+
+  equals = () => {
+    this.updateResult();
+    this.setState(p => ({
+      value: p.result
+    }));
   }
 
   render() {
+    const minusStyle = (this.state.opp === '-') ? ' selected' : '';
+    const plusStyle = (this.state.opp === '+') ? ' selected' : '';
+    const timesStyle = (this.state.opp === '*') ? ' selected' : '';
+    const divideStyle = (this.state.opp === '/') ? ' selected' : '';
+
     return (
       <Grid>
         <Row className="show-grid">
           <div className="calcDisplay">
-            {this.state.value} {(this.state.opp !== '=') && this.state.opp}
+            {this.state.value}
           </div>
         </Row>
         <Row className="show-grid">
@@ -108,25 +100,25 @@ class Calc extends React.Component {
             <button className="calcBtn" onClick={() => this.updateValue('7')}>7</button>
             <button className="calcBtn" onClick={() => this.updateValue('8')}>8</button>
             <button className="calcBtn" onClick={() => this.updateValue('9')}>9</button>
-            <button className="calcBtn" onClick={() => this.updateOpp('/')}>/</button>
+            <button className={"calcBtn" + divideStyle} onClick={() => this.updateOpp('/')}>/</button>
           </Col>
           <Col xs={12}>
             <button className="calcBtn" onClick={() => this.updateValue('4')}>4</button>
             <button className="calcBtn" onClick={() => this.updateValue('5')}>5</button>
             <button className="calcBtn" onClick={() => this.updateValue('6')}>6</button>
-            <button className="calcBtn" onClick={() => this.updateOpp('*')}>*</button>
+            <button className={"calcBtn" + timesStyle} onClick={() => this.updateOpp('*')}>*</button>
           </Col>
           <Col xs={12}>
             <button className="calcBtn" onClick={() => this.updateValue('1')}>1</button>
             <button className="calcBtn" onClick={() => this.updateValue('2')}>2</button>
             <button className="calcBtn" onClick={() => this.updateValue('3')}>3</button>
-            <button className="calcBtn" onClick={() => this.updateOpp('-')}>-</button>
+            <button className={"calcBtn" + minusStyle} onClick={() => this.updateOpp('-')}>-</button>
           </Col>
           <Col xs={12}>
             <button className="calcBtn" onClick={() => this.updateValue('0')}>0</button>
             <button className="calcBtn" onClick={() => this.updateValue('.')}>.</button>
-            <button className="calcBtn" onClick={this.Evaluate}>=</button>
-            <button className="calcBtn" onClick={() => this.updateOpp('+')}>+</button>
+            <button className="calcBtn" onClick={this.equals}>=</button>
+            <button className={"calcBtn" + plusStyle} onClick={() => this.updateOpp('+')}>+</button>
           </Col>
         </Row>
         <Row>
